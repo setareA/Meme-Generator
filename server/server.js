99a3a45d0444a5f59a6add77d3d68bbb089fc552
setare askari <setareaskari79@gmail.com>
2021-06-30 22:10:01 +0200

    db created

--- server/server.js
+++ server/server.js
@@ -1,9 +1,19 @@
 'use strict';
 
 const express = require('express');
+const morgan = require("morgan");
+const passport = require('passport');
+const passportLocal = require('passport-local');
+const session = require('express-session'); // session middleware
+
+const userDao = require('./user-dao');
+const dao = require("./dao"); // module for accessing the DB
+
 
 // init express
 const app = new express();
+app.use(morgan("dev"));
+app.use(express.json()); 
 const port = 3001;
 
 // activate the server
