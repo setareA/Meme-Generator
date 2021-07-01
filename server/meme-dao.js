22d0c4a5972b46363e2e62f2aca1c90bbd801a3d
setare askari <setareaskari79@gmail.com>
2021-07-01 18:11:19 +0200

    get public memes api done

--- server/meme-dao.js
+++ server/meme-dao.js
@@ -1,47 +1,85 @@
-'use strict';
-const db = require('./db');
-
+"use strict";
+const db = require("./db");
 
 exports.listMemes = () => {
-    return new Promise((resolve, reject) => {
-      const sql = "SELECT * FROM meme INNER JOIN field on meme.id = field.memeId INNER JOIN user on meme.user_id = user.user_id";
-      db.all(sql, [], (err, rows) => {
-        if (err) {
-          reject(err);
-          return;
+  return new Promise((resolve, reject) => {
+    const sql =
+      "SELECT * FROM meme INNER JOIN field on meme.id = field.memeId INNER JOIN user on meme.user_id = user.user_id";
+    db.all(sql, [], (err, rows) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      const memes = rows.map((e) => ({
+        id: e.id,
+        imgAddr: e.img_addr,
+        visibility: e.visibility,
+        userId: e.user_id,
+        userName: e.username,
+        userRealName: e.name,
+        title: e.title,
+        num_of_fields: e.num_of_fields,
+        text: e.text,
+        position: e.position,
+        field: [],
+      }));
+
+      const result = memes.reduce(function (r, meme) {
+        console.log("haha");
+        //console.log(r);
+        console.log(r[meme.id]);
+        r[meme.id] = r[meme.id] || {}; // if it's undefined set to {}
+        if (Object.keys(r[meme.id]).length === 0) {
+          r[meme.id] = meme;
+          r[meme.id].field.push({ text: meme.text, pos: meme.position });
+          delete r[meme.id].text;
+          delete r[meme.id].position;
+        } else {
+          r[meme.id].field.push({ text: meme.text, pos: meme.position });
         }
-        const memes = rows.map((e) => ({
-          id: e.id,
-          imgAddr: e.img_addr,
-          visibility: e.visibility,
-          userId: e.user_id,
-          userName:e.username,
-          userRealName: e.name,
-          title: e.title,
-          num_of_fields: e.num_of_fields,
-          text: e.text,
-          position: e.position,
-          field: [],
-        }));
-        
-        const result = memes.reduce(function (r, meme) {
-            //console.log("haha");
-           // console.log(r);
-            r[meme.id] = r[meme.id] || {};
-           // console.log("hehe");
-           // console.log(r);
-            if(Object.keys(r[meme.id]).length === 0){
-                r[meme.id] = meme;
-                r[meme.id].field.push({text: meme.text, pos:meme.position});
-                delete r[meme.id].text;
-                delete r[meme.id].position;
-            }
-            else{
-                r[meme.id].field.push({text: meme.text, pos:meme.position});
-            }
-            return r;
-        }, []);
-        resolve(result.filter((e)=>e!=null));
-      });
+        return r;
+      }, []);
+      resolve(result.filter((e) => e != null));
     });
-  };
+  });
+};
+
+exports.listPublicMemes = () => {
+  return new Promise((resolve, reject) => {
+    const sql =
+      "SELECT * FROM meme INNER JOIN field on meme.id = field.memeId INNER JOIN user on meme.user_id = user.user_id WHERE visibility = ?";
+    db.all(sql, ["public"], (err, rows) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      const memes = rows.map((e) => ({
+        id: e.id,
+        imgAddr: e.img_addr,
+        visibility: e.visibility,
+        userId: e.user_id,
+        userName: e.username,
+        userRealName: e.name,
+        title: e.title,
+        num_of_fields: e.num_of_fields,
+        text: e.text,
+        position: e.position,
+        field: [],
+      }));
+
+      const result = memes.reduce(function (r, meme) {
+        r[meme.id] = r[meme.id] || {}; // if it's undefined set to {}
+        if (Object.keys(r[meme.id]).length === 0) {
+          r[meme.id] = meme;
+          r[meme.id].field.push({ text: meme.text, pos: meme.position });
+          delete r[meme.id].text;
+          delete r[meme.id].position;
+        } else {
+          r[meme.id].field.push({ text: meme.text, pos: meme.position });
+        }
+        return r;
+      }, []);
+      resolve(result.filter((e) => e != null));
+    });
+  });
+};
--- server/server.js
+++ server/server.js
@@ -1,25 +1,28 @@
-'use strict';
+"use strict";
 
-const express = require('express');
+const express = require("express");
 const morgan = require("morgan");
-const passport = require('passport');
-const passportLocal = require('passport-local');
-const session = require('express-session'); // session middleware
+const passport = require("passport");
+const passportLocal = require("passport-local");
+const session = require("express-session"); // session middleware
 
-const userDao = require('./user-dao');
-const memeDao = require("./meme-dao"); 
+const userDao = require("./user-dao");
+const memeDao = require("./meme-dao");
 
-passport.use(new passportLocal.Strategy((username, password, done) => {
-  // verification callback for authentication
-  userDao.getUser(username, password).then(user => {
-    if (user)
-      done(null, user);
-    else
-      done(null, false, { message: 'Username or password wrong' });
-  }).catch(err => {
-    done(err);
-  });
-}));
+passport.use(
+  new passportLocal.Strategy((username, password, done) => {
+    // verification callback for authentication
+    userDao
+      .getUser(username, password)
+      .then((user) => {
+        if (user) done(null, user);
+        else done(null, false, { message: "Username or password wrong" });
+      })
+      .catch((err) => {
+        done(err);
+      });
+  })
+);
 
 // serialize and de-serialize the user (user object <-> session)
 // we serialize the user id and we store it in the session: the session is very small in this way
@@ -31,51 +34,68 @@ passport.serializeUser((user, done) => {
 // starting from the data in the session, we extract the current (logged-in) user
 passport.deserializeUser((id, done) => {
   console.log("deserialize user");
-  userDao.getUserById(id)
-    .then(user => {
+  userDao
+    .getUserById(id)
+    .then((user) => {
       done(null, user); // this will be available in req.user
-    }).catch(err => {
+    })
+    .catch((err) => {
       done(err, null);
     });
 });
 
-
 // init express
 const app = new express();
 app.use(morgan("dev"));
-app.use(express.json()); 
+app.use(express.json());
 const port = 3001;
 
 // custom middleware: check if a given request is coming from an authenticated user
 const isLoggedIn = (req, res, next) => {
-    if (req.isAuthenticated())
-      return next();
-  
-    return res.status(401).json({ error: 'not authenticated' });
-  }
-  
-  app.use(session({
-    secret: 'twitty',
-    resave: false,
-    saveUninitialized: false
-  }));
-  
-  // tell passport to use session cookies
-  app.use(passport.initialize());
-  app.use(passport.session());
+  if (req.isAuthenticated()) return next();
 
-  app.get("/api/memes" ,/* isLoggedIn, */(req, res) => {
-  
-     memeDao
-       .listMemes()
-       .then((meme) => {
-         res.status(200).json(meme);
-       })
-       .catch((error) => {
-         res.status(500).json(error.message);
-       });
-   });
-  
+  return res.status(401).json({ error: "not authenticated" });
+};
+
+app.use(
+  session({
+    secret: "twitty",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+// tell passport to use session cookies
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get(
+  "/api/memes",
+  /* isLoggedIn, */ (req, res) => {
+    /*
+     * TODO:  check logged in and creator type
+     */
+    memeDao
+      .listMemes()
+      .then((meme) => {
+        res.status(200).json(meme);
+      })
+      .catch((error) => {
+        res.status(500).json(error.message);
+      });
+  }
+);
+
+app.get("/api/memes/public", (req, res) => {
+  memeDao
+    .listPublicMemes()
+    .then((meme) => {
+      res.status(200).json(meme);
+    })
+    .catch((error) => {
+      res.status(500).json(error.message);
+    });
+});
 
 // activate the server
 app.listen(port, () => {
