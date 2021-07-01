86921705e3a88ecc0611ea10494c3da7fa18b852
setare askari <setareaskari79@gmail.com>
2021-07-01 22:45:53 +0200

    add login

--- server/server.js
+++ server/server.js
@@ -119,6 +119,39 @@ app.get(
   }
 );
 
+// login
+app.post("/api/sessions", function (req, res, next) {
+  passport.authenticate("local", (err, user, info) => {
+    if (err) return next(err);
+    if (!user) {
+      // display wrong login messages
+      return res.status(401).json(info);
+    }
+    // success, perform the login
+    req.login(user, (err) => {
+      if (err) return next(err);
+
+      // req.user contains the authenticated user, we send all the user info back
+      // this is coming from userDao.getUser()
+      return res.json(req.user);
+    });
+  })(req, res, next);
+});
+
+// DELETE /sessions/current
+// logout
+app.delete("/api/sessions/current", isLoggedIn, (req, res) => {
+  req.logout();
+  res.end();
+});
+
+// GET /sessions/current
+// check whether the user is logged in or not
+app.get("/api/sessions/current", isLoggedIn, (req, res) => {
+  console.log(req.user);
+  res.status(200).json(req.user);
+});
+
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
